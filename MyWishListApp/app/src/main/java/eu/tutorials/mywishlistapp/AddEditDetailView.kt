634df5f5cb1547dad0acde6186d42e6ea024a50f
Benyuan Cao <benyuancao@Benyuans-MacBook-Pro.local>
2024-01-29 23:37:47 -0500

    Day 14 of Android Development

--- MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
+++ MyWishListApp/app/src/main/java/eu/tutorials/mywishlistapp/AddEditDetailView.kt
@@ -15,12 +15,17 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonColors
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -34,18 +39,37 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import eu.tutorials.mywishlistapp.data.Wish
+import kotlinx.coroutines.launch
 
 @Composable
 fun AddEditDetailView(
     id: Long, viewModel: WishViewModel, navController: NavController
 ) {
-    Scaffold(topBar = {
-        AppBarView(
-            title = if (id != 0L) stringResource(id = R.string.update_wish) else stringResource(
-                id = R.string.add_wish
-            )
-        ) { navController.navigateUp() }
-    }) {
+    val snackMessage = remember {
+        mutableStateOf("")
+    }
+
+    val scope = rememberCoroutineScope()
+    val scaffoldState = rememberScaffoldState()
+    if(id != 0L){
+        val wish = viewModel.getAWishById(id).collectAsState(initial = Wish(0L,"",""))
+        viewModel.wishTitleState = wish.value.title
+        viewModel.wishDescriptionState = wish.value.description
+    } else {
+        viewModel.wishTitleState = ""
+        viewModel.wishDescriptionState = ""
+    }
+
+    Scaffold(
+        scaffoldState = scaffoldState,
+        topBar = {
+            AppBarView(
+                title = if (id != 0L) stringResource(id = R.string.update_wish) else stringResource(
+                    id = R.string.add_wish
+                )
+            ) { navController.navigateUp() }
+        }
+    ) {
         Column(
             modifier = Modifier
                 .padding(it)
@@ -71,14 +95,35 @@ fun AddEditDetailView(
                 })
 
             Spacer(modifier = Modifier.height(10.dp))
-            Button(onClick = {
-                if (viewModel.wishTitleState.isNotEmpty() && viewModel.wishDescriptionState.isNotEmpty()) {
-                    // update wish
-                } else {
-                    // Show toast
-                }
+            Button(
+                onClick = {
+                    if (viewModel.wishTitleState.isNotEmpty() && viewModel.wishDescriptionState.isNotEmpty()) {
+                        if (id != 0L) {
+                            viewModel.updateWish(
+                                Wish(
+                                    id = id,
+                                    title = viewModel.wishTitleState.trim(),
+                                    description = viewModel.wishDescriptionState.trim()
+                                )
+                            )
+                        } else {
+                            viewModel.addWish(
+                                Wish(
+                                    title = viewModel.wishTitleState.trim(),
+                                    description = viewModel.wishDescriptionState.trim()
+                                )
+                            )
+                            snackMessage.value = "A Wish has been created"
+                        }
+                    } else {
+                        snackMessage.value = "Enter fields to create a wish"
+                    }
+                    scope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar(snackMessage.value)
+                        navController.navigateUp()
+                    }
 
-            },
+                },
                 colors = ButtonDefaults.buttonColors(backgroundColor = Color.Blue),
                 shape = RoundedCornerShape(8.dp)
             ) {
